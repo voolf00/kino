5390a99920d27fa35a71f20ccae29371cf570f6c
voolf00 <voolf00@yandex.ru>
2014-10-09 02:03:47 +0400

    Signed-off-by: voolf00 <voolf00@yandex.ru>

--- film/tests.py
+++ film/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+import os
+import sys
 
-# Create your tests here.
+path = '/home/ubuntu/virtualenvs/p-k-kino/kino'
+
+if path not in sys.path:
+    sys.path.append(path)
+os.environ['DJANGO_STTINGS_MODUL'] = 'kKino.settings'
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
---
+++ kKino/django.wsgi
@@ -0,0 +1,6 @@
+import os
+import sys
+
+path = '/home/ubuntu/virtualenvs/p-k-kino/kino
+
+if path not in sys.path:
